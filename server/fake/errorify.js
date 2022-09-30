ded447143c25653b73dd43b7d975b0dd1ed5ed3c
ozoq <70523219+ozoq@users.noreply.github.com>
2022-09-30 21:42:23 +0200

    Fixed probablity by length in fake (lengthWeightedRandomField)

--- server/fake/errorify.js
+++ server/fake/errorify.js
@@ -12,28 +12,29 @@ import {
 } from "./helpers.js";
 
 /**
- * From an object where every value is a string, returns a random entry [name, value]
- * The larger the string in an entry, the higher the change to get its entry (relatively)
- * BUG: When a string length somehow gets to zero, no error, including insertCharacter() will ever proke
- *  A fix would be to first generate weights on a record, then errorify it many times
+ * From an object where every value is a string, returns a random field name
+ * The larger the string in an entry, the higher the change to get its field
  */
-const lengthWeightedRandomEntry = (faker) => (object) =>
-  compose(objectValuesLengthSum, faker.mersenne.rand, (stopAt) =>
+const lengthWeightedRandomField = (faker) => (object) =>
+  compose(objectTotalStringsLength, faker.mersenne.rand, (stopAt) =>
     Object.entries(object).reduce(
-      ({ lengthWalked, result }, entry) => ({
-        lengthWalked: lengthWalked + entry[1].length,
-        result: inProximity(stopAt, lengthWalked, entry[1].length)
-          ? entry
-          : result,
+      ({ walked, result }, [name, { length }]) => ({
+        walked: walked + length,
+        result: result || (inProximity(stopAt, walked, length) && name),
       }),
-      { lengthWalked: 0, result: null }
+      { walked: 0, result: null }
     )
   )(object).result;
 
 /**
+ * Given a field name, in an object, finds an entry of that field
+ */
+const getEntryByFieldName = (field) => (object) => [field, object[field]];
+
+/**
  * In an object where every value is a string, find the sum of these strings lengthes
  */
-const objectValuesLengthSum = (object) =>
+const objectTotalStringsLength = (object) =>
   Object.values(object).reduce((sum, value) => sum + value.length, 0);
 
 /**
@@ -59,18 +60,27 @@ const errorifyEntry = (faker) => (entry) =>
 /**
  * Apply errorifyEntry() to a random entry in an object
  */
-const errorifyObject = (faker) => (object) => ({
+const errorifyObject = (faker) => (initialObject) => (object) => ({
   ...object,
   ...compose(
-    lengthWeightedRandomEntry(faker),
+    getEntryByFieldName(
+      log(lengthWeightedRandomField(faker)(log(initialObject, 2)), 1)
+    ),
     errorifyEntry(faker),
     entryToObject
   )(object),
 });
+
+const log = (val, msg) => {
+  console.log(val, msg);
+  return val;
+};
 
 /**
  * Exported method errorifyObject() to provided object,
  *  this many times as provided by amount
  */
 export default (faker) => (amount) => (object) =>
-  repeat(probabilityToInteger(faker)(amount))(errorifyObject(faker))(object);
+  repeat(probabilityToInteger(faker)(amount))(errorifyObject(faker)(object))(
+    object
+  );
